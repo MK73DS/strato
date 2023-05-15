8d6496461121b556584e45a373dcbd5cf60cffee
MK73DS <10391562+MK73DS@users.noreply.github.com>
2023-05-15 17:32:03 +0200

    Do not tell the game if a memory chunk doesn't allow attribute change, only tell the logger 🤫

--- app/src/main/cpp/skyline/kernel/svc.cpp
+++ app/src/main/cpp/skyline/kernel/svc.cpp
@@ -63,9 +63,7 @@ namespace skyline::kernel::svc {
         }
 
         if (!chunk->state.attributeChangeAllowed) {
-            state.ctx->gpr.w0 = result::InvalidState;
             Logger::Warn("Attribute change not allowed for chunk: 0x{:X}", pointer);
-            return;
         }
 
         auto newChunk{*chunk};
